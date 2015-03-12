6facd8f356683555dd9ba74ef641be72d166bbba
Kyriakosk <kyriakosk5@gmail.com>
2015-03-12 21:43:54 +0000

    Update Client.java

--- src/uk/ac/ed/inf/op/model/Client.java
+++ src/uk/ac/ed/inf/op/model/Client.java
@@ -1,5 +1,14 @@
 package uk.ac.ed.inf.op.model;
 
 public class Client extends Person {
+  private int budget;
+  public Client(String fstName,String lstName, String email, int budget){
+    super(fstName,lstName,email);
+    this.budget = budget;
+  }
+  
+  public int getBudget(){
+    return budget;
+  }
 
 }
